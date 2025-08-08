a0ff17b6242a62e85ed454ba318d3faf783faa42
Somasri Patra <somasrimini2002@gmail.com>
2025-08-09 02:12:51 +0530

    Update my_chatbot.py

--- my_chatbot.py
+++ my_chatbot.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import streamlit as st
 
 st.title("My first chatbot!")
-genai.configure(api_key = "AIzaSyAcz_oLvwwsceMy_zheGZREW-VUmN9Y4oU")
+genai.configure(api_key = "---")
 model = genai.GenerativeModel("gemini-2.5-pro")
 
 user_prompt = st.text_input("Enter your question or prompt: ")
